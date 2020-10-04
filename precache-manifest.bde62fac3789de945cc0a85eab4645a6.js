a380dd99a635baf3c786cbb216035c8faaa0230e
hoangduy <duynguyen.joy@gmail.com>
2020-10-04 10:53:26 +0700

    Updates

---
+++ precache-manifest.bde62fac3789de945cc0a85eab4645a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4486855ca0748514ef94d1025e31633c",
+    "url": "/image-view/index.html"
+  },
+  {
+    "revision": "bf6023af3119e399c46b",
+    "url": "/image-view/static/css/main.515540a8.chunk.css"
+  },
+  {
+    "revision": "58ddbcdfedb41332a2f2",
+    "url": "/image-view/static/js/2.35e75de8.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/image-view/static/js/2.35e75de8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf6023af3119e399c46b",
+    "url": "/image-view/static/js/main.e69f6155.chunk.js"
+  },
+  {
+    "revision": "6f08dc7818b49de03958",
+    "url": "/image-view/static/js/runtime-main.aad68515.js"
+  }
+]);
